b87e353e7861cca8cd4a710a7efaf62a1b0e7731
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2014-05-28 13:50:38 -0300

    version beta

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ jquery-placeholder-label
 ========================
 
 A placeholder label for input
+
+Demo: http://fabiorogeriosj.com.br/plugin/jquery-placeholder-label/
+
+[![Screen](/example/demo.gif)](/example/demo.gif)
---
+++ src/jquery.placeholder.label.js
@@ -0,0 +1,100 @@
+(function ($) {
+    $.fn.placeholderLabel = function(options) {
+        var settings = $.extend({
+            // These are the defaults.
+            placeholderColor: "#898989",
+            labelColor: "#4AA2CC",
+            labelSize: this.css('font-size'),
+            useBorderColor: true,
+            inInput: true,
+            timeMove: 200
+        }, options);        
+        //Work
+        $(this).each(function (i,e){
+            var self = $(e);
+            var currentBorderColor = self.css('border-color');
+            var currentPlaceholderSize = self.css('font-size');
+            if(self.attr('placeholder')){
+            
+                var label = $('<label></label>');
+                label.css('position','absolute');
+                label.css('cursor','initial');
+                label.css('color',settings.placeholderColor);
+                label.css('font-size',currentPlaceholderSize);
+
+                var text = self.attr('placeholder');
+                self.removeAttr('placeholder');
+                label.text(text);
+                var ep = self.position().left;
+                var pt = self.css('padding-top');
+                var pl = self.css('padding-left');
+                var mt = self.css('margin-top');
+                var ml = self.css('margin-left');
+
+                label.css('margin-top', (Number(pt.replace('px',''))) + Number(mt.replace('px','')));
+                label.css('margin-left', (Number(pl.replace('px','')) - 5) + Number(ml.replace('px','')));
+                label.css('padding-left','5px');
+                label.css('padding-right','5px');
+                label.css('background-color',self.css('background-color'));
+
+                //Event
+                var self = self;
+                label.click(function (){
+                    self.focus();
+                });
+                self.focus(function(){
+                    if(settings.useBorderColor){
+                        self.css('border-color',settings.labelColor);
+                    }
+                    label.css('color',settings.labelColor);
+                    if(!self.val().length){
+                        var lh = label.height();
+                        var mtm = Number(pt.replace('px','')) + (lh/2);
+                        if(!settings.inInput){
+                            mtm += lh/2;
+                            label.css('background-color','');
+                        }
+                        label.animate({
+                            marginTop: "-="+mtm,
+                            fontSize: settings.labelSize,
+                        }, settings.timeMove);
+                    }
+                });
+                self.blur(function(){
+                    if(settings.useBorderColor){
+                        self.css('border-color',currentBorderColor);
+                    }
+                    label.css('color',settings.placeholderColor);
+                    if(!self.val().length){
+                        var lh = label.height();
+                        var mtm = Number(pt.replace('px','')) + (lh/2);
+                        if(!settings.inInput){
+                            mtm += lh/2;
+                            label.css('background-color','');
+                        }
+                        label.animate({
+                            marginTop: "+="+mtm,
+                            fontSize: currentPlaceholderSize
+                        }, settings.timeMove);
+                    }
+                });
+                if(self.attr('alt')){
+                    var textLabel = self.attr('alt');
+                    var textLabelOld = label.text();
+                    self.removeAttr('alt');
+                    self.keyup(function (){
+                        console.log(1)
+                        if(self.val().length){
+                            label.text(textLabel);
+                        } else {
+                            label.text(textLabelOld);
+                        }
+                    });
+                }
+                return self.before(label);
+            } else {
+                return null;
+            }
+        });
+    };
+}(jQuery));
